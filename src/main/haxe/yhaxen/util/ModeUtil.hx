67758a173b89577779d7ef712d231f5806ab75cc
Jozef Chutka <jozefchutka@gmail.com>
2014-12-30 13:29:01 +0100

    fixed mode rules

--- src/main/haxe/yhaxen/util/ModeUtil.hx
+++ src/main/haxe/yhaxen/util/ModeUtil.hx
@@ -4,10 +4,8 @@ class ModeUtil
 {
 	public static function matches(modes:Array<String>, mode:String):Bool
 	{
-		if(mode == null && modes == null)
+		if(modes == null)
 			return true;
-		if(mode != null && modes == null)
-			return false;
 		if(mode == null && modes != null)
 			return false;
 		return Lambda.has(modes, mode);
--- src/test/haxe/yhaxen/util/ModeUtilTest.hx
+++ src/test/haxe/yhaxen/util/ModeUtilTest.hx
@@ -33,13 +33,13 @@ class ModeUtilTest
 	@Test
 	public function testExample():Void
 	{
-		Assert.isTrue(ModeUtil.matches(["a"], "a"));
+		Assert.isTrue(ModeUtil.matches(null, "a"));
 		Assert.isTrue(ModeUtil.matches(null, null));
+		Assert.isTrue(ModeUtil.matches(["a"], "a"));
 		Assert.isTrue(ModeUtil.matches(["a", "b", "c"], "a"));
 		Assert.isTrue(ModeUtil.matches(["a", "b", "c"], "b"));
 		Assert.isTrue(ModeUtil.matches(["a", "b", "c"], "c"));
 
-		Assert.isFalse(ModeUtil.matches(null, "a"));
 		Assert.isFalse(ModeUtil.matches([], "a"));
 		Assert.isFalse(ModeUtil.matches(["a", "b", "c"], "d"));
 		Assert.isFalse(ModeUtil.matches(["a", "b", "c"], null));
